cc5502918a9f5077e19a3c7f9db6ffe020123acc
King Kong <king-kong@gmail.com>
2022-11-08 14:26:11 -0800

    1-15 done gave up on 16 to work on other things

--- exercise_11/exercise_11.sql
+++ exercise_11/exercise_11.sql
@@ -3,3 +3,16 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+SELECT
+    film.title,
+    count(rental.rental_id)
+FROM
+    film
+    LEFT JOIN inventory on film.film_id = inventory.film_id
+    LEFT JOIN rental on inventory.inventory_id = rental.inventory_id
+group by
+    film.film_id
+order by
+    count desc
+limit
+    10
